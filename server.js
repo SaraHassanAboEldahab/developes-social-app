cdec48f06e350ee68b047659667df68e7b9fee94
SaraHassanAboEldahab <sara.aboeldahab205@gmail.com>
2020-12-11 10:40:18 +0200

    first editing

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require("express")
 const connectDB = require("./config/db")
+const path = require("path")
 
 const app = express()
 
-//connect to database
+//Connect to database
 connectDB()
 
-//init middleware
+//Init middleware
 app.use(express.json({ extended: false }))
 
-//define routes
+//Define routes
 app.use("/api/users", require("./routes/api/users"))
 app.use("/api/profile", require("./routes/api/profile"))
 app.use("/api/auth", require("./routes/api/auth"))
 app.use("/api/posts", require("./routes/api/posts"))
 
+//Define static assets in production
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("client/build"))
 
-const port = process.env.PORT || 5000;
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
-app.listen(port, () => console.log(`server started on ${port}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`))
